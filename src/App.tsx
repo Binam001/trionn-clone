59cca72fe86a3f5a6cd6ee115e0244829206cfb5
Binam001 <binamshrestha770@gmail.com>
2025-11-10 09:17:20 +0545

    added smokey-cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import SmokeyCursor from "./components/lightswind/smokey-cursor";
 import Navbar from "./components/Navbar";
 import LandingPage from "./pages/LandingPage";
 import gsap from "gsap";
@@ -8,6 +9,13 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 function App() {
   return (
     <div className="px-3 md:px-10 lg:px-28 bg-(--background)">
+      <SmokeyCursor
+        simulationResolution={64}
+        dyeResolution={512}
+        densityDissipation={5}
+        velocityDissipation={3}
+        enableShading={false}
+      />
       <Navbar />
       <div className="pt-36">
         <LandingPage />

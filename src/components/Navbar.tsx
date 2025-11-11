900c5a1a5f31f83ef8fe5cec0c860cbda918e9cd
Binam001 <binamshrestha770@gmail.com>
2025-11-11 20:56:23 +0545

    fixed navbar padding

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,7 +25,7 @@ const Navbar = () => {
     setIsPlaying(!isPlaying);
   };
   return (
-    <nav className="fixed top-0 left-0 right-0 px-3 md:px-10 lg:px-20 z-50">
+    <nav className="fixed top-0 left-0 right-0 px-3 md:px-10 lg:px-28 z-50">
       <div className="flex justify-between items-center mt-5 lg:mt-10">
         <div className="cursor-pointer">
           <a href="#text1">
